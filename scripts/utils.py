a8ec5900c93a99652b2d9e3a3ab773a80e83bd38
Andrés Campaña Otero <andrescaot@gmail.com>
2023-05-31 18:08:39 +0200

    Implement a function to save a model state dict to a path

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,6 +1,22 @@
+from pathlib import Path
+import torch
 from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
 import os
+
+def save_model(model,
+               target_dir,
+               model_name):
+  target_dir_path = Path(target_dir)
+  target_dir_path.mkdir(parents=True,
+                        exist_ok=True)
+
+  assert model_name.endswith(".pth") or model_name.endswith(".pt"), "model_name should end with '.pt' or '.pth'"
+  model_save_path = target_dir_path / model_name
+
+  print(f"[INFO] Saving model to: {model_save_path}")
+  torch.save(obj=model.state_dict(),
+             f=model_save_path)
 
 def create_writer(experiment_name, 
                   model_name, 
