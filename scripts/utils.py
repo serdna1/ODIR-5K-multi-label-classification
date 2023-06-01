09f96cdfacc94d8c0630c86da062a853363738aa
Andrés Campaña Otero <andrescaot@gmail.com>
2023-06-02 01:06:45 +0200

    Create a function to split a dfin train, val and test dfs and save them in a dir

--- scripts/utils.py
+++ scripts/utils.py
@@ -3,6 +3,8 @@ import torch
 from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
 import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 def save_model(model,
                target_dir,
@@ -31,3 +33,19 @@ def create_writer(experiment_name,
     print(f"[INFO] Created SummaryWriter, saving to: {log_dir}...")
     
     return SummaryWriter(log_dir=log_dir)
+
+def split_annotations(annotations_path, target_dir_path, test_ratio = 0.1, val_ratio = 0.2, test_split = True, seed = 42):
+    odir_df = pd.read_excel(annotations_path)
+    
+    target_dir_path.mkdir(parents=True, exist_ok=True)
+    
+    if test_split:
+        train_df, test_df = train_test_split(odir_df, test_size = test_ratio, random_state = seed)
+        train_df, val_df = train_test_split(train_df, test_size = val_ratio, random_state = seed)
+        
+        test_df.to_excel(target_dir_path / 'test_annotations.xlsx', index = False)        
+    else:    
+        train_df, val_df = train_test_split(odir_df, test_size = val_ratio, random_state = seed)
+        
+    train_df.to_excel(target_dir_path / 'train_annotations.xlsx', index = False)
+    val_df.to_excel(target_dir_path / 'val_annotations.xlsx', index = False)
