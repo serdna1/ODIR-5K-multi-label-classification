3782826e48860ee588e84a5505a07bf8beb1ae82
Andrés Campaña Otero <andrescaot@gmail.com>
2023-06-08 23:22:35 +0200

    Change log_dir format of the SummaryWriter

--- scripts/utils.py
+++ scripts/utils.py
@@ -23,12 +23,10 @@ def save_model(model,
 def create_writer(experiment_name, 
                   model_name, 
                   extra):
-    timestamp = datetime.now().strftime("%Y-%m-%d")
-
     if extra:
-        log_dir = os.path.join("runs", timestamp, experiment_name, model_name, extra)
+        log_dir = os.path.join('..', 'runs', experiment_name, model_name, extra)
     else:
-        log_dir = os.path.join("runs", timestamp, experiment_name, model_name)
+        log_dir = os.path.join('..', 'runs', experiment_name, model_name)
         
     print(f"[INFO] Created SummaryWriter, saving to: {log_dir}...")
     
