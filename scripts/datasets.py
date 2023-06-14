2e70a907315aa8f775238020746c07e03d10efee
Andrés Campaña Otero <andrescaot@gmail.com>
2023-06-14 19:40:48 +0200

    Get rid of the usage example on the datasets file

--- scripts/datasets.py
+++ scripts/datasets.py
@@ -1,11 +1,7 @@
-from pathlib import Path
 from PIL import Image
 import numpy as np
-import pandas as pd
 import torch
-from torchvision import transforms
 from torch.utils.data import Dataset
-from sklearn.model_selection import train_test_split
 
 class ODIRDataset(Dataset):
     def __init__(self, images_path, df, transform=None, join_images=False):
@@ -36,31 +32,3 @@ class ODIRDataset(Dataset):
             return pair_img, target
         
         return (left_img, right_img), target
-
-# Usage example
-if __name__ == '__main__':
-    images_path = 'data/images'
-    annotations_path = 'data/annotations.xlsx'
-
-    transform = transforms.Compose([
-        transforms.Resize(size=(512, 512)),
-        transforms.ToTensor(),
-    ])
-    
-    odir_df = pd.read_excel(annotations_path)
-
-    test_ratio = 0.1
-    val_ratio = 0.2
-
-    label_names = ['A','C','D','G','H','M','N','O']
-    
-    train_df, test_df = train_test_split(odir_df, test_size=test_ratio, random_state=42)
-    train_df, val_df = train_test_split(train_df, test_size=val_ratio, random_state=42)
-
-    train_dataset = ODIRDataset(images_path, train_df, transform, join_images=True)
-    val_dataset = ODIRDataset(images_path, val_df, transform, join_images=True)
-    test_dataset = ODIRDataset(images_path, test_df, transform, join_images=True)
-
-    print(f'Train dataset lenght: {len(train_dataset)}')
-    print(f'Validation dataset lenght: {len(val_dataset)}')
-    print(f'Test dataset lenght: {len(test_dataset)}')
