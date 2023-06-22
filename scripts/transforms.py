29ed59b965d6ca517737a9f10ab72970e1378e87
Andrés Campaña Otero <andrescaot@gmail.com>
2023-06-22 02:28:36 +0200

    Add transforms FOVExtraction and CircleCrop

--- scripts/transforms.py
+++ scripts/transforms.py
@@ -1,24 +1,39 @@
+import cv2
 import numpy as np
 from PIL import Image
 
-class GetROI(object):
-    """Crops black pixels in a retinography (gets the FOV)"""
+class FOVExtraction(object):
+    
+    def __call__(self, img, tol=7):
+        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR) # PIL to cv
+        
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        mask = gray_img > tol
+        img_blue = img[:, :, 0][np.ix_(mask.any(1), mask.any(0))]
+        img_green = img[:, :, 1][np.ix_(mask.any(1), mask.any(0))]
+        img_red = img[:, :, 2][np.ix_(mask.any(1), mask.any(0))]
+        img = np.stack([img_blue, img_green, img_red], axis=-1)
+            
+        img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)) # cv to PIL
+        
+        return img
+
+
+class CircleCrop(object):
 
     def __call__(self, img):
-        img = np.array(img)
+        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR) # PIL to cv
 
-        # Mask of coloured pixels.
-        mask = img > 0
+        height, width, _ = img.shape
 
-        # Coordinates of coloured pixels.
-        coordinates = np.argwhere(mask)
+        x = int(width/2)
+        y = int(height/2)
+        r = np.amin((x, y))
 
-        # Binding box of non-black pixels.
-        x0, y0, s0 = coordinates.min(axis=0)
-        x1, y1, s1 = coordinates.max(axis=0) + 1 # slices are exclusive at the top.
+        circle_img = np.zeros((height, width), np.uint8)
+        cv2.circle(circle_img, (x, y), int(r), 1, thickness=-1)
+        img = cv2.bitwise_and(img, img, mask=circle_img)
 
-        # Get the contents of the bounding box.
-        img = img[x0:x1, y0:y1]
-        img = Image.fromarray(img)
+        img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)) # cv to PIL
         
         return img
